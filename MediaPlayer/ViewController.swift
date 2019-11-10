55cbb5fb6b95040608081f438f20c433be00e2e1
Mandar Choudhary <mandar.developer7@gmail.com>
2019-11-10 20:47:15 +0530

    Initial Commit

---
+++ MediaPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaPlayer
+//
+//  Created by Mandar Choudhary on 10/11/19.
+//  Copyright © 2019 Mandar Choudhary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
